4abe0e37e9b29ac7cfe868909007650501c6103f
Panelpandaex <pandaex151@gmail.com>
2024-12-10 13:11:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "6rQ13CYD#2o2OO0Eqo111Aq2es0oSir9rFAjb4E_dF3PhIIGPkn8", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
